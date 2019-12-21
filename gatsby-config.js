cfe750d55e69f003d3511b6fc85eff81a97d4c1e
Brian <brianhh1230@gmail.com>
2019-12-20 23:01:06 -0500

    initial pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
+const dotenv = require("dotenv");
+
+if (process.env.ENVIRONMENT !== "production") {
+  dotenv.config();
+}
+
+const { spaceId, accessToken } = process.env;
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Nerdy Python`,
+    description: `Hobbyist Ball Python Breeders and Keepers`,
+    author: `@nerdypython`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -12,6 +20,13 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId,
+        accessToken
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
