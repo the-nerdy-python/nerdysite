5dc02b2927daae52b45e61d782951d40933002c9
Brian <brianhh1230@gmail.com>
2019-12-29 09:54:07 -0500

    working webchat via gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,25 +1,22 @@
-import React from "react";
-export function onRenderBody(
-  { setPostBodyComponents }) {
-    setPostBodyComponents([
-     <div id="webchat"/>,
-     <script
-        type="text/javascript"
-        src="https://storage.googleapis.com/mrbot-cdn/webchat-latest.js"
-      />,
-     <script
-     dangerouslySetInnerHTML={{
-        __html:`
-        WebChat.default.init({
-            selector: "#webchat",
-            initPayload: "/help",
-            customData: {"language": "en"}, // arbitrary custom data. Stay minimal as this will be added to the socket
-            socketUrl: "https://woz.thenerdypython.com",
-            socketPath: "/socket.io/",
-            title: "Nerdy Chat",
-          })
-   	 `
-     }}
-     />,
-]);
+import React from "react"
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    <div id="webchat"/>,
+    <script src="https://storage.googleapis.com/mrbot-cdn/webchat-latest.js"></script>,
+    <script
+        dangerouslySetInnerHTML={{
+            __html: `
+            WebChat.default.init({
+                selector: "#webchat",
+                initPayload: "/help",
+                customData: {"language": "en"}, // arbitrary custom data. Stay minimal as this will be added to the socket
+                socketUrl: "https://woz.thenerdypython.com",
+                socketPath: "/socket.io/",
+                title: "Nerdy Chat",
+                })
+            `,
+        }}
+    />
+  ])
 }
